0d9390a6463c94d6629b82fb9c24d61ba24aebc7
Chris Siebenmann <cks.git01@cs.toronto.edu>
2012-10-10 15:24:29 -0400

    Commit of initial DTrace scripts et al

---
+++ iscsi-trace.d
@@ -0,0 +1,233 @@
+#!/usr/sbin/dtrace -qFs
+#pragma D option quiet
+#pragma D option defaultargs
+/*
+ * Report on iSCSI initiator IO timings and so on.
+ *
+ * usage: iscsi-dtrace.d [verbosity [slow-msecs]]
+ *
+ * verbosity 1 produces per-LUN+target IP statistics as well as some
+ * other attempts to track queue depth.
+ *
+ * verbosity 2 also reports details on long iSCSI IOs. By default it
+ * reports on anything that took 500 msec or longer; a second argument
+ * (in milliseconds) changes that.
+*/
+
+/*
+ * CDDB command byte values I have seen:
+ *	 0	0x00	TEST_UNIT_READY
+ *	 8	0x08	READ
+ *	26	0x1a	MODE_SENSE
+ *	37	0x25	READ_CAPACITY
+ *	40	0x28	READ_G1
+ *	42	0x2a	WRITE_G1
+ *     160	0xa0	REPORT_LUNS
+ */
+
+BEGIN
+{
+	verbose = $1;
+	closedcnt = 0;
+	starttime = timestamp;
+	/* The default delay time is 500 msec */
+	delaytime = $2 > 0 ? $2 * 1000000 : 500 * 1000000;
+}
+
+/*
+ * State E1 is the initial creation of the iSCSI request.
+ * State E2 is request transmitted.
+ * State E3 is completion from the network.
+ * State E8 is full completion of SCSI commands.
+ * States E4 and E6 are various sorts of errors. We don't see them in normal
+ * operation; I'm including them just in case.
+ */
+
+sdt::iscsi_cmd_state_machine:event
+/(string) arg2 == "E1"/
+{
+	/* started[arg0] = timestamp; */
+	e3[arg0] = 0;
+	@counts["submit (all cmd types)"] = count();
+	@submit["submit cmd type", ((iscsi_cmd_t *) arg0)->cmd_type] = count();
+}
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E1"/
+{
+	started[arg0] = timestamp;
+}
+
+/*
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && ((string) arg2 == "E1" || started[arg0]) /
+{
+	this->icmdp = (iscsi_cmd_t *) arg0;
+	printf("%u delta %8u %p %d %s %s\n", timestamp, timestamp - started[arg0], arg0, this->icmdp->cmd_type, (string) arg1, (string) arg2);
+}
+*/
+
+sdt::iscsi_cmd_state_machine:event
+/verbose && ((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E2" && started[arg0]/
+{
+	this->delta = (timestamp - started[arg0])/250000;
+	@trans["transmit scsi", "1/4 ms"] = quantize(this->delta); 
+
+	this->icmdp = (iscsi_cmd_t *) arg0;
+	this->sess = this->icmdp->cmd_lun->lun_sess;
+	@simulcnt["outstanding connection commands"] = lquantize(this->icmdp->cmd_conn->conn_queue_active.count, -1, 33);
+	@simulcnt["pending session commands"] = lquantize(this->sess->sess_queue_pending.count, -1, 33);
+	@simulcnt["session window"] = lquantize(this->sess->sess_maxcmdsn - this->sess->sess_cmdsn, -1, 33);
+	@simulcnt["session table count"] = lquantize(this->sess->sess_cmd_table_count, -1, 33);
+	closedcnt += (this->sess->sess_maxcmdsn - this->sess->sess_cmdsn) > 0 ? 0 : 1;
+}
+
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E2" && started[arg0]/
+{
+	@counts["transmit scsi"] = count();
+	trans[arg0] = timestamp;
+}
+
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && ((string) arg2 == "E4" || (string) arg2 == "E6") && started[arg0]/
+{
+	@counts2["error handling", (string) arg2] = count();
+	this->delta = (timestamp - started[arg0])/1000000;
+	@errors["state transition to", (string) arg2, "ms"] = quantize(this->delta);
+	started[arg0] = 0;
+	trans[arg0] = 0;
+}
+
+sdt::iscsi_cmd_state_machine:event
+/verbose && ((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E3" && started[arg0] && trans[arg0] /
+{
+	this->icmdp = (iscsi_cmd_t *) arg0;
+	this->delta2 = (timestamp - started[arg0])/250000;
+	this->delta = (timestamp - started[arg0])/1000000;
+
+	/* cid + oid is more useful for identifying things, I think.
+	   it shows up in kstat output et al. */
+	/* @lrg["completion", this->icmdp->cmd_conn->conn_cid, this->icmdp->cmd_conn->conn_oid, "1/4 ms"] = quantize(this->delta); */
+	/* @lrg["completion", (string) this->icmdp->cmd_lun->lun_guid, "1/4 ms"] = quantize(this->delta); */
+
+	this->tgtname = (string) this->icmdp->cmd_lun->lun_sess->sess_name;
+	this->addr =  (u_char *) &this->icmdp->cmd_conn->conn_curr_addr.sin4.sin_addr;
+	@lrg["completion for target:", this->tgtname, this->icmdp->cmd_lun->lun_num, (uint) this->addr[0], (uint) this->addr[1], (uint) this->addr[2], (uint) this->addr[3], "1/4 ms"] = quantize(this->delta2);
+	@tgtavg["completion for target:", this->tgtname, this->icmdp->cmd_lun->lun_num, (uint) this->addr[0], (uint) this->addr[1], (uint) this->addr[2], (uint) this->addr[3], "avg ms"] = avg(this->delta);
+
+	/* this->cmdb = this->icmdp->cmd_un.scsi.pkt->pkt_cdbp[0];
+	// this->cdb = this->icmdp->cmd_un.scsi.pkt->pkt_cdbp;
+	// this->lba = this->cdb[2] << 24 | this->cdb[3] << 16 | this->cdb[4] << 8 | this->cdb[5];
+	// this->lba = this->lba / (2097152*2);
+	// @lba["lba addresses quantized in 2 GB chunks", this->cmdb] = lquantize(this->lba, 0, 100);
+	*/
+}
+
+sdt::iscsi_cmd_state_machine:event
+/verbose >= 2 && ((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E3" && started[arg0] && (timestamp - started[arg0]) > delaytime /
+{
+	this->icmdp = (iscsi_cmd_t *) arg0;
+	this->lun = this->icmdp->cmd_lun;
+	this->tgtname = (string) this->lun->lun_sess->sess_name;
+	this->delta = (timestamp - started[arg0])/1000000;
+	/* See uts/common/sys/scsi/impl/commands.h; we want group 1, 2 format.
+	   also io/scsi/targets/sd.c */
+	this->cmdb = this->icmdp->cmd_un.scsi.pkt->pkt_cdbp[0];
+	this->cdb = this->icmdp->cmd_un.scsi.pkt->pkt_cdbp;
+	this->lba = (uint)this->cdb[2] << 24 | (uint)this->cdb[3] << 16 | (uint)this->cdb[4] << 8 | (uint)this->cdb[5];
+	this->count = (uint)this->cdb[7] << 8 | (uint)this->cdb[8];
+	this->addr =  (u_char *) &this->icmdp->cmd_conn->conn_curr_addr.sin4.sin_addr;
+	printf("LONG ISCSI %p %s/%d @ %d.%d.%d.%d delta %d ms CDDB cmd %2d clen %d slen %d data trans %6d lba %d count %d\n", arg0, this->tgtname, this->lun->lun_num, (uint) this->addr[0], (uint) this->addr[1], (uint) this->addr[2], (uint) this->addr[3], this->delta, this->cmdb, this->icmdp->cmd_un.scsi.cmdlen, this->icmdp->cmd_un.scsi.statuslen, this->icmdp->cmd_un.scsi.data_transferred, this->lba, this->count);
+	/* @slowlba["lba for slow IO with CDDB byte", this->cmdb] = quantize(this->lba); */
+}
+
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E3" && started[arg0]/
+{
+	this->icmdp = (iscsi_cmd_t *) arg0;
+	this->delta2 = (timestamp - started[arg0])/250000;
+	this->delta = (timestamp - started[arg0])/1000000;
+	@complete["completion", "ms"] = lquantize(this->delta, 0, 100, 1);
+	@spread["low-res completion", "ms"] = lquantize(this->delta, 50, 1000, 50);
+	@spread2["intermediate completion", "ms"] = lquantize(this->delta, 0, 200, 5);
+
+	@counts["scsi complete"] = count();
+	e3[arg0] = timestamp;
+	@xavg["avg completion", "1/4 ms"] = avg(this->delta2);
+	@xmin["min completion", "1/4 ms"] = min(this->delta2);
+	@xmax["max completion", "ms"] = max(this->delta);
+
+	/* 40 is SCMD_READ_G1, 42 is SCMD_WRITE_G1 */
+	/* See uts/common/sys/scsi/generic/commands.h */
+	this->cmdb = this->icmdp->cmd_un.scsi.pkt->pkt_cdbp[0];
+	@ccount["SCSI CDDB command byte", this->cmdb] = count();
+	@cmax["SCSI CDDB command byte", this->cmdb, "max ms"] = max(this->delta);
+	@cavg["SCSI CDDB command byte", this->cmdb, "avg ms"] = avg(this->delta);
+	@cdist["completion for CDDB command byte:", this->cmdb, "(ms)"] = quantize(this->delta);
+
+	/* This is what really matters */
+	@recvdelta["transmit to receive time", "ms"] = quantize((timestamp - trans[arg0])/1000000);
+	trans[arg0] = 0;
+}
+
+sdt::iscsi_cmd_state_machine:event
+/verbose && ((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E8" && started[arg0]/
+{
+	this->delta2 = e3[arg0] ? (timestamp - e3[arg0])/1000 : -1;
+	@delta["E3 to E8 time delta", "us"] = quantize(this->delta2);
+	e3[arg0] = 0;
+}
+
+sdt::iscsi_cmd_state_machine:event
+/((iscsi_cmd_t *) arg0)->cmd_type == 1 && (string) arg2 == "E8" && started[arg0]/
+{
+	this->delta = (timestamp - started[arg0])/500000;
+	/* surplus given what is generally a very close tie between E3 and E8. */
+	/* @["done", "1/2 ms"] = lquantize(this->delta, 0, 200, 1); */
+	@counts["scsi done"] = count();
+	started[arg0] = 0;
+}
+
+END
+{
+	printf("\nRUN TIME: %d seconds\n\n", (timestamp - starttime) / 1000000000);
+	printf("\nItem counts:\n");
+	printa("%-25s   %@10d\n", @counts); printf("\n");
+	printa("%s %1d:   %@10d\n", @submit); printf("\n");
+	printa("%s %3d:   %@10d\n", @ccount); printf("\n");
+	printf("(CDDB command byte 40 is READ_G1, 42 is WRITE_G1. 0, 8, 26, and 37 are\nunimportant)\n");
+
+	printf("\nStats:\n");
+	printa("%-20s %6s:  %@8d\n", @xavg);
+	printa("%-20s %6s:  %@8d\n", @xmin);
+	printa("%-20s %6s:  %@8d\n", @xmax);
+	printf("\n");
+	printa("%-20s %3d %s: %@4d\n", @cavg); printf("\n");
+	printa("%-20s %3d %s: %@4d\n", @cmax);
+
+	printf("\nDetail timing distribution:");
+	printa(@complete);
+	printa(@spread);
+	printa(@spread2);
+	printf("(CDDB command byte 40 is READ_G1, 42 is WRITE_G1)\n");
+	printa("%s %d %s %@d\n", @cdist); printf("\n");
+	printa(@recvdelta);
+}
+
+END
+/verbose/
+{
+	printf("\nPer-target completion (split by IP)\n");
+	printa("%22s %34s/%d %d.%d.%d.%d -- %s: %@4d\n", @tgtavg); printf("\n");
+	printa("%s %s/%d %d.%d.%d.%d in %s: %@d\n", @lrg);
+
+	printf("\nObscure:");
+	printa(@trans);
+	printa(@delta);
+
+	printf("\nCounts of outstanding & pending commands & stuff\n");
+	printf("Session window was closed: %d times\n", closedcnt);
+	printa(@simulcnt);
+
+	/* printa(@lba); */
+}
---
+++ nfs3-dtrace.d
@@ -0,0 +1,330 @@
+#!/usr/sbin/dtrace -s
+#pragma D option quiet
+#pragma D option defaultargs
+#pragma D option dynvarsize=64m
+/*
+ * Report on NFS v3 and ZFS IO latencies and timings.
+ *
+ * usage: nfs3-dtrace.d [verbosity]
+ *
+ * Verbosity 2 prints stack traces that you probably don't care about.
+ *
+ * High-level NFS and ZFS IO is separated into 'fast' IO (takes less than
+ * 1/4 msec) and 'slow' IO. The assumption is that fast IO is being
+ * satisfied from cache instead of from actual disk IO.
+ */
+
+dtrace:::BEGIN
+{
+	ziotype[0] = "zio_null";
+	ziotype[1] = "zio_read";
+	ziotype[2] = "zio_write";
+	ziotype[3] = "zio_free";
+	ziotype[4] = "zio_claim";
+	ziotype[5] = "zio_ioctl";
+
+	chain[0] = "(error)";
+	chain[1] = "zfs_read";
+	chain[2] = "zfs_write";
+	chain[3] = "zfs_write from commit";
+
+	verbose = $1;
+	starttime = timestamp;
+}
+
+/* 
+ * NFS v3 tracing
+*/
+fbt::rfs3_read:entry
+{self->rts = timestamp;}
+fbt::rfs3_read:return 
+/self->rts > 0 && (timestamp-self->rts) > 250000/
+{
+	this->delta = (timestamp - self->rts)/1000000;
+	@nfs3["rfs3_read for > 1/4 ms times", "ms"] = quantize(this->delta);
+	@avg["NFS v3 read avg", "ms"] = avg(this->delta);
+	@max["NFS v3 read max", "ms"] = max(this->delta);
+}
+fbt::rfs3_read:return 
+/self->rts > 0/
+{
+	this->str = (timestamp - self->rts) > 250000 ? "slow reads" : "fast reads";
+	@count[this->str] = count();
+	self->rts = 0;
+}
+
+fbt::rfs3_write:entry
+{self->wts = timestamp;}
+fbt::rfs3_write:return 
+/self->wts > 0 && (timestamp-self->wts) > 250000/
+{
+	this->delta = (timestamp - self->wts)/1000000;
+	@nfs3["rfs3_write for > 1/4 ms times", "ms"] = quantize(this->delta);
+	@avg["NFS v3 write avg", "ms"] = avg(this->delta);
+	@max["NFS v3 write max", "ms"] = max(this->delta);
+}
+fbt::rfs3_write:return 
+/ self->wts > 0 /
+{
+	this->str = (timestamp - self->wts) > 250000 ? "slow writes" : "fast writes";
+	@count[this->str] = count();
+	self->wts = 0;
+}
+
+fbt::rfs3_commit:entry
+{self->cts = timestamp;}
+fbt::rfs3_commit:return 
+/self->cts > 0 && (timestamp-self->cts) > 250000/
+{
+	this->delta = (timestamp - self->cts)/1000000;
+	@nfs3["rfs3_commit for > 1/4 ms times", "ms"] = quantize(this->delta);
+	@avg["NFS v3 commit avg", "ms"] = avg(this->delta);
+	@max["NFS v3 commit max", "ms"] = max(this->delta);
+}
+fbt::rfs3_commit:return 
+/ self->cts > 0 /
+{
+	this->str = (timestamp - self->cts) > 250000 ? "slow commits" : "fast commits";
+	@count[this->str] = count();
+	self->cts = 0;
+}
+
+fbt::rfs3_*:entry
+/ probefunc != "rfs3_readdirplus_free" && probefunc != "rfs3_read_free" && probefunc != "rfs3_readlink_free" && probefunc != "rfs3_readdir_free" /
+{ 
+	self->basets = timestamp;
+}
+fbt::rfs3_*:return
+/ self->basets > 0 /
+{
+	this->delta = (timestamp - self->basets) / 1000000;
+	@opcount[probefunc] = count();
+	@opavg[probefunc, "avg ms"] = avg(this->delta);
+	@opdist[probefunc, "ms"] = quantize(this->delta);
+	self->basets = 0;
+}
+
+/*
+ * ZFS level tracing
+ *
+ * zfs_(read|write) is the high level ZFS interface, zio_* is the low
+ * level IO to ZFS devices that is not satisfied from cache. <cks>'s
+ * understanding is that ZIO stuff cascades down vdevs; you issue a
+ * ZIO against the top level vdev and then it's reflected or replicated
+ * to the low-level vdevs that represent actual disks.
+ */
+
+fbt::zfs_read:entry, fbt::zfs_write:entry
+{
+	self->zts = timestamp;
+	self->chain = 0;
+	@zcount[probefunc] = count();
+}
+fbt::zfs_read:return, fbt::zfs_write:return
+/ self->zts > 0 /
+{
+	this->delta = (timestamp - self->zts) / 1000000;
+	@zavg[probefunc, "avg ms"] = avg(this->delta);
+	@zmax[probefunc, "max ms"] = max(this->delta);
+	@zstats[probefunc, "ms"] = quantize(this->delta);
+	self->ztz = 0;
+	self->chain = 0;
+}
+
+/* We ignore zio_ioctl (io_type 5) operations because they're only
+   used for DKIOCFLUSHWRITECACHE operations and our disks don't
+   support those so they complete immediately (in Solaris, they
+   don't result in iSCSI commands). We don't need the noise that
+   results from tracking them; we only care about real operations
+   that might hit the disk.
+*/
+fbt::zio_create:return
+/ args[1]->io_type && args[1]->io_type != 5/
+{
+	this->op = ziotype[args[1]->io_type];
+	ziots[args[1]] = timestamp;
+	@ziocount[this->op] = count();
+	ziostart[args[1]] = 0;
+	ziointr[args[1]] = 0;
+}
+/* Okay, we'll count zio_ioctl operations as indicators of
+   theoretically sync operations but otherwise exclude them.
+   I'm neurotic. */
+fbt::zio_create:return
+/ args[1]->io_type && args[1]->io_type == 5 /
+{
+	this->op = ziotype[args[1]->io_type];
+	@ziocount[this->op] = count();
+}
+
+fbt::zio_done:entry
+/ ziots[args[0]] > 0 /
+{
+	this->delta = (timestamp - ziots[args[0]]) / 1000000;
+	this->op = ziotype[args[0]->io_type];
+	@zioavg[this->op, "ZIO op avg ms"] = avg(this->delta);
+	@ziomax[this->op, "ZIO op max ms"] = max(this->delta);
+	@ziostats[this->op, "ms"] = quantize(this->delta);
+	ziots[args[0]] = 0;
+	ziostart[args[0]] = 0;
+}
+
+/* We only do the delay on these if it's larger than 1/4 msec.
+   NOTE: it's possible for this to come out with zio_vdev_io_start
+   having a higher time than zio_interrupt. To highlight this we
+   count events too.
+
+   Otherwise too noisy. This is after zio_done to make things
+   print out in the right order (sigh). */
+fbt::zio_vdev_io_start:entry, fbt::zio_interrupt:entry
+/ ziots[args[0]] > 0 && (timestamp - ziots[args[0]]) > 250000 /
+{
+	this->delta = (timestamp - ziots[args[0]]) / 1000000;
+	this->op = ziotype[args[0]->io_type];
+	@ziointercount[probefunc, this->op, "for >1/4ms ops"] = count();
+	@ziointeravg[probefunc, this->op, "avg ms (for >1/4 ms ops)"] = avg(this->delta);
+	@ziointerstats[probefunc, this->op, "ms (for >1/4 ms ops)"] = quantize(this->delta);
+}
+
+/*
+ * Deltas from one operation to another.
+ */
+fbt::zfs_read:entry
+/ self->rts > 0 /
+{ 
+	this->delta = (timestamp - self->rts)/1000000;
+	@delta["rfs3_read to zfs_read", "ms"] = quantize(this->delta);
+	self->chain = 1;
+}
+fbt::zfs_write:entry
+/ self->wts > 0 /
+{ 
+	this->delta = (timestamp - self->wts)/1000000;
+	@delta["rfs3_write to zfs_write", "ms"] = quantize(this->delta);
+	self->chain = 2;
+}
+fbt::zfs_write:entry
+/ self->cts > 0 /
+{ 
+	this->delta = (timestamp - self->cts)/1000000;
+	@delta["rfs3_commit to zfs_write", "ms"] = quantize(this->delta);
+	self->chain = 3;
+}
+
+/* We attempt to only track actual synchronous operations,
+   not async IO kicked off (later) from one of these operations. We
+   do this based on priority (args[0]), a zio_priority_table[] entry;
+   we want priority 0. 
+   We exclude zio_ioctl operations for the same reason as before: no
+   real disk IO is produced.
+*/
+/* args0 is zio_t *pio, args8 is zio_type_t type. */
+fbt::zio_create:entry
+/ args[8] && args[8] != 5 && args[9] == 0 && self->zts > 0 && self->chain/
+{
+	this->delta = (timestamp - self->zts)/1000000;
+	@zdelta["zio_create (sync) from", chain[self->chain], "ms"] = quantize(this->delta);
+}
+
+/*
+ * Attempt to track 'slow' ZIO initiation. This is the creation of a ZIO
+ * operation that starts more than 20 milliseconds after whatever higher-level
+ * operation started the show.
+ * The stack traces here are not very useful in practice.
+*/
+fbt::zio_create:entry
+/ verbose >= 2 && args[8] && args[8] != 5 && args[9] == 0 && self->zts > 0 && self->chain && ((timestamp - self->zts)/1000000) > 20/
+{
+	@stack[ziotype[args[8]], ">20 msec sync zio_create from", chain[self->chain], stack()] = count();
+}
+
+fbt::zio_vdev_io_start:entry
+/ ziots[args[0]] > 0 /
+{
+	ziostart[args[0]] = timestamp;
+}	
+
+/*
+ * This is the ZFS-level view of the underlying IO latency; it goes from
+ * when the vdev IO is started until ZIO is signalled that it's done.
+ */
+fbt::zio_interrupt:entry
+/ ziostart[args[0]] > 0 /
+{
+	this->delta = (timestamp - ziostart[args[0]]) / 1000000;
+	this->op = ziotype[args[0]->io_type];
+	/* This is actually a crucial measure of the actual low-level device
+	 * latency as observed by ZFS.
+	 */
+	@zdelta1["zio_vdev_io_start() to zio_interrupt() for", this->op, "ms"] = quantize(this->delta);
+	ziostart[args[0]] = 0;
+	ziointr[args[0]] = timestamp;
+}
+fbt::zio_done:entry
+/ ziointr[args[0]] > 0 /
+{
+	this->delta = (timestamp - ziointr[args[0]]) / 250000;
+	this->op = ziotype[args[0]->io_type];
+	@zdelta2["interrupt to done", this->op, "1/4 ms"] = quantize(this->delta);
+	ziointr[args[0]] = 0;
+}
+
+/*
+ * Print things usefully.
+ */
+END
+{
+	printf("\nRUN TIME: %d seconds\n\n", (timestamp - starttime) / 1000000000);
+	printf("\n== Event counts ==");
+	printf("\nAll NFS v3 ops:\n");
+	printa("  %-15s   %@10d\n", @opcount);
+	printf("\n\nNFS v3 read/write/commit; fast is <1/4 msec, assumed to hit cache:\n");
+	printa("  %-15s   %@10d\n",@count);
+	printf("\n\nZFS/ZIO counts:\n");
+	printa("  %-15s   %@10d\n", @zcount); printf("\n");
+	printa("  %-15s   %@10d\n", @ziocount);
+
+	printf("\n\n== Averages and other stats ==\n\n");
+	printa("  %-30s   %@10d %s\n", @avg); printf("\n");
+	printa("  %-30s   %@10d %s\n", @max); printf("\n");
+	printa("  %-20s %9s   %@10d\n", @opavg); printf("\n");
+	printa("  %-20s %9s   %@10d\n", @zavg); printf("\n");
+	printa("  %-20s %9s   %@10d\n", @zmax); printf("\n");
+	printa("  %-10s %19s   %@10d\n", @zioavg); printf("\n");
+	printa("  %-10s %19s   %@10d\n", @ziomax);
+
+	printf("\n\nNFS v3 read/write/commit that took >1/4 msec:");
+	printa(@nfs3);
+
+	printf("\nNFS durations for all NFS operations, all times:");
+	printa(@opdist);
+
+	printf("\nZFS high-level operations:");
+	printa(@zstats);
+	printf("\nZIO durations for various sorts of ZIO ops:");
+	printa(@ziostats);
+	printf("\nCore 'disk' delay as seen by ZIO:\n");
+	printa("%s %s in %s: %@d\n", @zdelta1);
+
+	printf("\n\n == ");
+	printf("\nTime delay distributions for various things to other things:");
+	printa(@delta); printf("\n");
+	printa("%s %s in %s: %@d\n", @zdelta); printf("\n");
+
+	printf("\n'Slow' intermediate ZIO counts/averages/distributions:\n");
+	printf("These are for time deltas (from zio_create()) that are > 1/4 msec.\n\n");
+	printa("  %-18s for %-9s           %@8d\n", @ziointercount); printf("\n");
+	printa("  %-18s for %-9s   avg ms: %@8d\n", @ziointeravg); printf("\n");
+	printa("%s for ZIO type %s in %s: %@d\n", @ziointerstats);
+	printf("\n  This reports zio_vdev_io_start() or zio_interrupt() calls that took more\n  than 1/4 msec to go from zio_create() to the routine. (Note that it's\n  routine for slow zio_vdev_io_start() to average higher than zio_interrupt().\n");
+
+	printf("\nZIO zio_interrupt() to zio_done() time delay:\n");
+	printa("%s for %s in %s: %@d\n", @zdelta2);
+	printf("\n\n ---- \n");
+}
+
+END
+/ verbose >= 2 /
+{
+	printa(@stack);
+}
