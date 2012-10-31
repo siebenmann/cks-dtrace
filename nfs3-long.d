9bfe0aefe3c85e1e568b877151ff54a32c720e56
Chris Siebenmann <cks.git01@cs.toronto.edu>
2012-10-31 12:12:16 -0400

    nfs3-long.d: add support for all NFS v3 operations
    We now can report on basically all long NFS v3 operations, not just
    read/write/commit operations.

--- nfs3-long.d
+++ nfs3-long.d
@@ -8,11 +8,25 @@
  *
  * usage: nfs3-long.d [verbosity [delay]]
  *
+ * verbosity 0 is the default. 'nfs3-long.d 0 <delay>' is valid to just
+ * set the delay.
  * verbosity 1 shows filenames when available.
  * verbosity 2 also shows currently active request counts on a per-pool
  *	basis.
  * delay is in milliseconds and defaults to 500 msec.
  *
+ * <when> LONG NFS ...		- long NFS read/write/commit operations;
+ *				  it's normal for 'commit' operations to
+ *				  report 0 bytes.
+ * <when> LONG NOP ...		- long other NFS operations; the operation
+ *				  is reported but there is no IO size et al.
+ *
+ * A 'txg +<number>' is reported if a transaction group committed in the
+ * ZFS pool between the start and the end of the operation. The <number>
+ * is how many transaction groups committed.
+ *
+ * ASSUMPTION: all exported filesystems are ZFS filesystems. This is
+ * true in our environment.
  */
 
 BEGIN
@@ -43,6 +57,24 @@ BEGIN
 						/* (the default/normal) */
 	wtypes[1] = "write.data_sync";		/* DATA_SYNC */
 	wtypes[2] = "write.file_sync";		/* FILE_SYNC */
+
+	/* Names for NFS v3 operations we track the times of */
+	nametbl["rfs3_readlink"]	= "readlink";
+	nametbl["rfs3_access"]		= "access";
+	nametbl["rfs3_create"]		= "create";
+	nametbl["rfs3_link"]		= "link";
+	nametbl["rfs3_rename"]		= "rename";
+	nametbl["rfs3_remove"]		= "remove";
+	nametbl["rfs3_mkdir"]		= "mkdir";
+	nametbl["rfs3_mknod"]		= "mknod";
+	nametbl["rfs3_rmdir"]		= "rmdir";
+	nametbl["rfs3_symlink"]		= "symlink";
+	nametbl["rfs3_setattr"]		= "setattr";
+	nametbl["rfs3_readdir"]		= "readdir";
+	nametbl["rfs3_readdirplus"]	= "readdirplus";
+	nametbl["rfs3_access"]		= "access";
+	nametbl["rfs3_lookup"]		= "lookup";
+	nametbl["rfs3_getattr"]		= "getattr";
 
 	delaytime = $2 > 0 ? $2 * 1000000 : 500 * 1000000;
 	verbose = $1;
@@ -160,6 +192,7 @@ fbt::rfs3_read:return, fbt::rfs3_write:return, fbt::rfs3_commit:return
 	self->req = 0;
 	self->uid = 0;
 	self->dir = 0;
+	self->fs = 0;
 	self->ts = 0;
 	self->vp = 0;
 	self->path = 0;
@@ -173,4 +206,73 @@ fbt::rfs3_read:return, fbt::rfs3_write:return, fbt::rfs3_commit:return
 END
 {
 	trunc(@poolc);
+}
+
+/*
+ * Mass tracing of timing for various ops.
+ * To add an operation, add it to nametbl[] in BEGIN and then add a
+ * fbt::<op-func>:entry trace here. However, <cks> believes that we
+ * cover essentially every NFS v3 operation of interest already.
+ *
+ * We rely on the nfs3_fhtovp:return tracing above to retrieve the
+ * ZFS pool information and other things attached to the vnode for
+ * the operation.
+ */
+fbt::rfs3_readlink:entry,fbt::rfs3_access:entry,fbt::rfs3_create:entry,fbt::rfs3_link:entry,fbt::rfs3_rename:entry,fbt::rfs3_remove:entry,fbt::rfs3_mkdir:entry,fbt::rfs3_mknod:entry,fbt::rfs3_rmdir:entry,fbt::rfs3_symlink:entry,fbt::rfs3_setattr:entry,fbt::rfs3_readdir:entry,fbt::rfs3_readdirplus:entry,fbt::rfs3_access:entry,fbt::rfs3_lookup:entry,fbt::rfs3_getattr:entry
+{
+	self->in_rfs3 = 1;
+	self->req = args[3];
+	self->uid = args[4]->cr_uid;
+	self->fs = (string)args[2]->exi_export.ex_path;
+	self->mts = timestamp;
+
+	this->socket =
+	    (struct sockaddr_in *)self->req->rq_xprt->xp_xpc.xpc_rtaddr.buf;
+	/* DTrace 1.0: no inet functions, no this->strings */
+	this->a = (uint8_t *)&this->socket->sin_addr.S_un.S_addr;
+	this->addr1 = strjoin(lltostr(this->a[0] + 0ULL), strjoin(".",
+	    strjoin(lltostr(this->a[1] + 0ULL), ".")));
+	this->addr2 = strjoin(lltostr(this->a[2] + 0ULL), strjoin(".",
+	    lltostr(this->a[3] + 0ULL)));
+	self->address = strjoin(this->addr1, this->addr2);
+	self->address = names[self->address] != 0 ? names[self->address] : self->address;
+}
+
+fbt::rfs3_*:return
+/ self->mts > 0 && (timestamp - self->mts) > delaytime /
+{
+	this->delta = (timestamp - self->mts) / 1000000;
+	this->path = strjoin(" file ", self->path);
+	this->istr = self->inum ? strjoin(" inum ", lltostr(self->inum)) : "";
+
+	/* We would like to directly dereference the znode, but it may not
+	   exist any more by the time we reach this routine. So we must get
+	   the current pool txg through a hack. */
+	this->ctxg = pooltxg[self->pool];
+	this->tstr = this->ctxg != self->itxg ?
+		strjoin(" txg +", lltostr(this->ctxg-self->itxg)) : "";
+
+	printf("%Y LONG NOP %5d msec %s on %s %s for uid %d from %s%s%s%s\n",
+		walltimestamp, this->delta, 
+		nametbl[probefunc] != 0 ? nametbl[probefunc] : probefunc,
+		self->fs, self->pool, self->uid, self->address, 
+		this->istr, this->tstr, 
+		verbose && self->path != "" ? this->path : "");
+}
+
+fbt::rfs3_*:return
+/ self->mts > 0 /
+{
+	self->req = 0;
+	self->uid = 0;
+	self->fs = 0;
+	self->mts = 0;
+	self->in_rfs3 = 0;
+	self->ts = 0;
+	self->vp = 0;
+	self->path = 0;
+	self->znode = 0;
+	self->inum = 0;
+	self->pool = 0;
+	self->itxg = 0;
 }
